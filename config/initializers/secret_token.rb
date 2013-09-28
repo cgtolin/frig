0d35e1cbf2231366084c20c3097cbb1db1c19e46
Chris Tolin <chris.tolin@clickbank.com>
2013-09-27 19:31:11 -0600

    Baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Frig::Application.config.secret_key_base = 'a20df115ddf2466f6f6ac73d15616f4315405bd22a54c79144a3764920149ed4647a2dc1c1fb2674bcf097f75ed8e3e2299d08d0a6017c596768e6e2dd2added'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Frig::Application.config.session_store :cookie_store, key: '_frig_session'
